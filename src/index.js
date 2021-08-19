5eededd811f4560cd1e85673f98829354d6d60b8
Tito <ebenirot@gmail.com>
2021-08-19 14:36:19 +0100

    Code factoring

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
-const app = require("express")()
+const express = require("express")
+const app = express()
 const mongoose = require("mongoose")
 const path = require("path")
-const Contact = require("/model/contact.js")
+const Contact = require("./model/contact.js")
 
-const dotenv = require("dotenv");
+require("dotenv").config();
 
-dotenv.config();
+const { MONGO_URI } = process.env
+console.log(MONGO_URI)
 mongoose.connect(
-	process.env.MONGO_URI,
+	MONGO_URI,
 	{
 		useNewUrlParser: true,
 		useUnifiedTopology: true,
@@ -18,17 +20,30 @@ mongoose.connect(
 
 app.use(express.json());
 
-app.get("/tito-ebeniro", (req, res) => {
-    res.sendFile(path.join(__dirname+'/resume/index.html'));
+app.get("/tito-ebeniro", async (req, res) => {
+    try {
+        res.sendFile(path.join(__dirname+'/resume/index.html'));
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({ message: error });
+    }
 })
 
 app.post("/tito-ebeniro", async (req, res) => {
-    let contact = req.body;
+    const {name, email, subject, message} = req.body;
     try {
-        const result = Contact.save(contact)
-        return res.status(200).json({ message: "Message sucessfully sent.", contact});
+        if (!name || !email || !subject || !message) {
+            return res.status(500).json({ message: "Incomplete data." })
+        }
+        const contact = req.body;
+        await Contact.create(contact)
+        console.log(contact)
+
+        return res.status(200).json({ message: "Email sent successfully." })
+        // res.sendFile(path.join(__dirname+'/resume/index.html'));
     } catch (error) {
-        return res.status(500).json({ message: error });
+        console.log(error.message)
+        return res.status(500).json({ message: "Email not sent." });
     }
 })
 
