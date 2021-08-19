6074699006204a15820acb208a692bf4cf067bc1
Tito <ebenirot@gmail.com>
2021-08-19 10:25:30 +0100

    Added MongoDB

--- src/index.js
+++ src/index.js
@@ -1,9 +1,35 @@
-app = require("express")()
-mongoose = require("mongoose")
-path = require("path")
+const app = require("express")()
+const mongoose = require("mongoose")
+const path = require("path")
+const Contact = require("/model/contact.js")
 
-app.get("/", (req, res) => {
+const dotenv = require("dotenv");
+
+dotenv.config();
+mongoose.connect(
+	process.env.MONGO_URI,
+	{
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		useFindAndModify: false,
+	},
+	() => console.log("MongoDB connected")
+);
+
+app.use(express.json());
+
+app.get("/tito-ebeniro", (req, res) => {
     res.sendFile(path.join(__dirname+'/resume/index.html'));
+})
+
+app.post("/tito-ebeniro", async (req, res) => {
+    let contact = req.body;
+    try {
+        const result = Contact.save(contact)
+        return res.status(200).json({ message: "Message sucessfully sent.", contact});
+    } catch (error) {
+        return res.status(500).json({ message: error });
+    }
 })
 
 app.listen(4100, ()=> {
